75ed7a8a7ef12ca8cc9ca1cf89b85c507d4b8186
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-06 17:04:41 +0300

    updated script file

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 import { dogs } from './data.js'
 
-let randomNumber = Math.floor(Math.random() * dogs.length) 
+
 const dogImage = document.getElementById("dog-image")
 const modalTtext = document.getElementById("modal-text")
 
+let randomNumber = 0
+
 document.addEventListener("click", (e) => {
+   
    if (e.target.id === 'cross-btn') {
     document.getElementById('nope-badge').style.display = 'block'
     document.getElementById("like-badge").style.display = 'none'
+    dogs[randomNumber].hasBeenSwiped = true
+
    } else if (e.target.id === 'like-btn') {
     document.getElementById('nope-badge').style.display = 'none'
     document.getElementById("like-badge").style.display = 'block'
+    dogs[randomNumber].hasBeenLiked = true
    }
-    
+
+   setTimeout(() => {
+    document.getElementById("like-badge").style.display = 'none'
+    document.getElementById('nope-badge').style.display = 'none'
+    randomNumber ++
+    renderInitialPhoto()
+    renderDogDescription()
+   }, 1000)
+
 })
 
 function renderInitialPhoto() {
+    
     let dogHtml = 
     `<img class="dog-img" src="${dogs[randomNumber].avatar}" alt="">
     `
@@ -25,6 +40,7 @@ function renderInitialPhoto() {
 renderInitialPhoto()
 
 function renderDogDescription() {
+    
     let dogTextHtml = 
     `<h3 class="dog-name-description">${dogs[randomNumber].name}, ${dogs[randomNumber].age}</h3>
     <p class="dog-message">${dogs[randomNumber].bio}</p>
@@ -33,3 +49,6 @@ function renderDogDescription() {
 }
 
 renderDogDescription()
+
+
+
